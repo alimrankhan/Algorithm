fb49d17b6892cc1cc0ea70275f04f34eeb8c2119
alimrankhan <72828652+alimrankhan@users.noreply.github.com>
2022-10-31 18:31:09 +0000

    c

--- seive_phi_single.py
+++ seive_phi_single.py
@@ -5,11 +5,11 @@ def seive_phi(n):
 	lim= int(math.sqrt(n))
 	phi= n
 	for i in range(2,lim+1):
-		if(n%i==0):
-			while(n%i == 0): #primeNum->i
-				n/=i #n= n/i
-				
-			phi= phi/i*(i-1) #phi(n)= n*(1-1/i)= n*(i-1/i)= n/i*(i-1)
+		#if(n%i==0):
+		while(n%i == 0): #primeNum->i
+			n/=i #n= n/i
+			
+		phi= phi/i*(i-1) #phi(n)= n*(1-1/i)= n*(i-1/i)= n/i*(i-1)
 	if n>1:
 		phi= phi/n*(n-1)
 	return int(phi)
