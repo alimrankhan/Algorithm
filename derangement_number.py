474cecdd61ccd043b37be53fa83e0a171a5d7069
Imran Khan <alimrankhan54@gmail.com>
2023-01-19 00:12:02 +0600

    derange number

---
+++ derangement_number.py
@@ -0,0 +1,16 @@
+#dearangement number
+
+def derangement_num(n:int):
+    if n==2:
+        return 1
+
+    if n==1:
+        return 0
+
+    return (n-1)*derangement_num(n-2) + (n-1)*derangement_num(n-1)
+
+print(derangement_num(3))
+
+print(derangement_num(4))
+
+print(derangement_num(5))
