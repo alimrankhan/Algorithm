a3d17b234f9ffb1ebc0cb54dd3253ccf721c416b
Imran Khan <alimrankhan54@gmail.com>
2024-04-02 03:27:17 +0600

    permutation modified

--- permutation.py
+++ permutation.py
@@ -11,7 +11,7 @@ print(f"The total number of elements is: {permutation(3,2)}")
 #generating actual permutation
 print('permutations using backtracking: ')
 visited= [0]*3
-li= [1,2,3]
+li= [5 ,2,3]
 ans= []
 def perm(li:list, pos:int):
     
@@ -20,12 +20,13 @@ def perm(li:list, pos:int):
         for i in ans:
             print(i, end=' ')
         print()
+        # ans.clear()
         
 
     for i in range(0,3):
         if not visited[i]:
             visited[i]= 1
-            ans.append(i+1)
+            ans.append(li[i])
             perm(li, pos+1)
             visited[i]= 0
             ans.pop()
