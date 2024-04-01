749f38e3635f2b68144c0babdc46affd4e71699b
Imran Khan <alimrankhan54@gmail.com>
2024-04-02 00:59:48 +0600

    comb

--- permutation.py
+++ permutation.py
@@ -6,4 +6,33 @@ def permutation(n:int, r:int):
 
     return ans
 
-print(permutation(3,2))
+print(f"The total number of elements is: {permutation(3,2)}")
+
+#generating actual permutation
+print('permutations using backtracking: ')
+visited= [0]*3
+li= [1,2,3]
+ans= []
+def perm(li:list, pos:int):
+    
+    if pos==len(li):
+        # return ans
+        for i in ans:
+            print(i, end=' ')
+        print()
+        
+
+    for i in range(0,3):
+        if not visited[i]:
+            visited[i]= 1
+            ans.append(i+1)
+            perm(li, pos+1)
+            visited[i]= 0
+            ans.pop()
+
+
+    return ans
+
+# li= [1,2,3] #output: [[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]]
+le= len(li)
+perm(li,0)
